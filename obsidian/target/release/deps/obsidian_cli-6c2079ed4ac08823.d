d1f80855e9d0327cfaffdca7b722016f16c9521a
The Builder <github-actions[bot]@users.noreply.github.com>
2026-08-17 19:52:23 +0000

    builder: obsidian: untrack build artifacts and ignore target/

--- obsidian/target/release/deps/obsidian_cli-6c2079ed4ac08823.d
+++
@@ -1,7 +0,0 @@
-/home/runner/work/Random/Random/obsidian/target/release/deps/obsidian_cli-6c2079ed4ac08823.d: crates/obsidian-cli/src/main.rs crates/obsidian-cli/src/bench.rs crates/obsidian-cli/src/cli.rs
-
-/home/runner/work/Random/Random/obsidian/target/release/deps/obsidian_cli-6c2079ed4ac08823: crates/obsidian-cli/src/main.rs crates/obsidian-cli/src/bench.rs crates/obsidian-cli/src/cli.rs
-
-crates/obsidian-cli/src/main.rs:
-crates/obsidian-cli/src/bench.rs:
-crates/obsidian-cli/src/cli.rs:
