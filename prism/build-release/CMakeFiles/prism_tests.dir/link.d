e4a753c012f5b66284fdeca752ddbd5cb389766d
The Builder <github-actions[bot]@users.noreply.github.com>
2026-09-01 17:14:04 +0000

    I just escalated to the Maintainer. The build run is complete with a negative result - the JXL-modular real encoder hit its structural ceiling at 3.291 bpp.
    **What I need:** Owner/Maintainer direction on next path (e.g., two-pass architecture, neural codec #226, or other approach to close the 4% gap to M2).
    
    No further action until directed.

---
+++ prism/build-release/CMakeFiles/prism_tests.dir/link.d
@@ -0,0 +1,199 @@
+prism_tests: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_crc32.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_bitstream.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_color.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_predict.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_rans.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_acoder.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_mixer.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_matree.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_analyze.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_squeeze.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_xband.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_blend.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_bias.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_tokenize.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_staticmodel.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_container.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_roundtrip.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_fuzz_gate.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_transform.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_histogram.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_hybrid_uint.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_ans_static.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_multipass.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_acoder_hybrid.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_x0_wavelet.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_predictor.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_r6b.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_r6c.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_r6d.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_r7.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_option_c.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_neural_codec.cpp.o \
+  CMakeFiles/prism_tests.dir/tests/unit/test_neural_entropy.cpp.o \
+  libprism_core.a \
+  lib/libgtest_main.a \
+  lib/libgtest.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_crc32.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_bitstream.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_color.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_predict.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_rans.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_acoder.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_mixer.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_matree.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_analyze.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_squeeze.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_xband.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_blend.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_bias.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_tokenize.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_staticmodel.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_container.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_roundtrip.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_fuzz_gate.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_transform.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_histogram.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_hybrid_uint.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_ans_static.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_multipass.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_acoder_hybrid.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_x0_wavelet.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_predictor.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_r6b.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_r6c.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_r6d.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_r7.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_option_c.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_neural_codec.cpp.o:
+
+CMakeFiles/prism_tests.dir/tests/unit/test_neural_entropy.cpp.o:
+
+libprism_core.a:
+
+lib/libgtest_main.a:
+
+lib/libgtest.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
